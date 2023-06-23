307fdad72c7d9fa07260974b7878534a42182ecb
kripa <kripa.kaliyadan@skill-lync.com>
2023-06-23 14:51:36 +0530

    third  commit

---
+++ functionss.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+void printingg();
+
+int main()
+{
+	printf("iam just trying function");
+	printingg();
+}
+void printingg()
+{
+	int a,b;
+ printf("enter two numbvers\n");
+ scanf("%d%d",&a,&b);
+ printf("a and b from function is %d %d ",a,b);
+}
+
