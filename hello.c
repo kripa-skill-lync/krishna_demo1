223a30ddfda799210c70fdf0b0ac4198f465fbc3
kripa <kripa.kaliyadan@skill-lync.com>
2023-06-23 14:46:01 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,38 @@
+clear
+
+
+
+
+cd..
+'
+
+
+grep skill hello.c///search for skill in hello.c
+grep -n skill hello.c///it shows the line number
+grep -c skill hello.c//how many times word is repeated(1 ,2 or 3 times etc)
+ctrl c to come out of asking for user input
+ls -la
+permission bits
+
+
+
+ssh key generation link verify
+
+https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+
+
+ssh-keygen -t ed25519 -C "your_email@example.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+cd .ssh
+ls
+cat td_ed567856.pub
+copy ssh key 
+go to git hub
+settingd-ssh key 
+new ssh key
+give name newkey to sshh
+paste
+save...
+key will be aaded
+
